35fc702c52ac63be1ecb1c8f47d62f304d07416b
Qasim <QasimK@users.noreply.github.com>
2013-08-02 21:47:47 +0100

    Item actions are now functions

--- src/wotw_project/game/item_actions.py
+++ src/wotw_project/game/item_actions.py
@@ -14,46 +14,25 @@ The target must be a Character (for now).
 
 from wotw_project.game import models
 
-class ItemAction:
-    """Don't create these objects"""
-    allow_character_target = False
-    allow_fight_target = False
-    #allow_environment_target = False
-    allow_in_combat = False
-    allow_out_combat = False
-    
-    def __new__(self):
-        raise NotImplementedError
-    
-    @classmethod
-    def __do__(cls, char, item, target):
-        raise NotImplementedError
 
-
-class IASelfHealOnce(ItemAction):
+def IASelfHealOnce(char, item, target):
     """Heal the target and destroy the item
     
     Uses the property "health healed"
     """
     
-    allow_character_target = True
-    allow_in_combat = True                                
-    allow_out_combat = True
-    
-    @classmethod
-    def __do__(cls, char, item, target):
-        if target.hp != target.max_hp:
-            heal = item.prop_health_healed()
-            target.hp = min(target.max_hp, target.hp + heal)
-            target.save()
-            target.inventory.remove_item(item)
-            
-            msg = "You ate the whatever and healed upto {} HP.".format(heal)
-            models.Message.objects.create(target=target, body=msg)
-        else:
-            err = "You are already at full hp."
-            models.Message.objects.create(target=target, body=err)
-            return err
+    if target.hp != target.max_hp:
+        heal = item.prop_health_healed()
+        target.hp = min(target.max_hp, target.hp + heal)
+        target.save()
+        target.inventory.remove_item(item)
+        
+        msg = "You ate the whatever and healed upto {} HP.".format(heal)
+        models.Message.objects.create(target=target, body=msg)
+    else:
+        err = "You are already at full hp."
+        models.Message.objects.create(target=target, body=err)
+        return err
 
 
 ITEM_ACTIONS = {
