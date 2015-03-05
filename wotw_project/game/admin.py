c877328fbe506b61eecbb6827ec277beb939c736
QasimK <QasimK@users.noreply.github.com>
2015-03-05 15:49:23 +0000

    Update admin interface

--- wotw_project/game/admin.py
+++ wotw_project/game/admin.py
@@ -1,26 +1,16 @@
 from django.contrib import admin
+from django.core.urlresolvers import reverse
 
-from game import models
-
-def wepinfo(obj):
-    s = "%s (%s)" % (str(obj.weapon), str(obj.weapon.prop_damage))
-    if obj.weapon.is_soulbound:
-        s += "*"
-    return s
-wepinfo.short_description = "Weapon (damage)"
-
-def arminfo(obj):
-    s = "%s (%s)" % (str(obj.armour), str(obj.armour.prop_damage_absorbed))
-    if obj.armour.is_soulbound:
-        s += "*"
-    return s
-arminfo.short_description = "Armour (damage absorbed)"
+from game.models import *
 
 
-
-class MonsterAdmin(admin.ModelAdmin):
-    list_display = ('name', 'hp', wepinfo, arminfo, 'gold')
-
+class ItemPropertyInfoInline(admin.TabularInline):
+    model = ItemProperty
+    extra = 1
+    
+class ItemItemActionInfoInline(admin.TabularInline):
+    model = ItemItemActionInfo
+    extra = 1
 
 def item_properties(item):
     props = item.itemproperty_set.all()
@@ -36,32 +26,42 @@ def item_actions(item):
         s += "[{}] ".format(iiai.display_text)
     return s
 
-class ItemPropertyInfoInline(admin.TabularInline):
-    model = models.ItemProperty
-    extra = 1
-    
-class ItemItemActionInfoInline(admin.TabularInline):
-    model = models.ItemItemActionInfo
-    extra = 1
-
+@admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
     list_display = ('name', 'is_unlimited_stack', 'max_stack_size',
                    'is_soulbound', item_properties, item_actions)
     search_fields = ('name',)
     list_filter = ('itemproperty__name', 'is_unlimited_stack')
-    fields = ('name', ('is_unlimited_stack', 'max_stack_size'),
-              ('is_soulbound',))#, 'item_actions')
+    fields = (
+        ('name',),
+        ('is_unlimited_stack', 'max_stack_size'),
+        ('is_soulbound',)
+    )
     inlines = [ItemPropertyInfoInline, ItemItemActionInfoInline]
 
-#class ItemPropertyAdmin(admin.ModelAdmin):
-#    list_display = ('name',)
 
-class InventoryItemInfoInline(admin.TabularInline):
-    model = models.InventoryItemInfo
-    extra = 0
+def wepinfo(obj):
+    s = "%s (%s)" % (str(obj.weapon), str(obj.weapon.prop_damage))
+    if obj.weapon.is_soulbound:
+        s += "*"
+    return s
+wepinfo.short_description = "Weapon (damage)"
+
+def arminfo(obj):
+    s = "%s (%s)" % (str(obj.armour), str(obj.armour.prop_damage_absorbed))
+    if obj.armour.is_soulbound:
+        s += "*"
+    return s
+arminfo.short_description = "Armour (damage absorbed)"
+
+@admin.register(Monster)
+class MonsterAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+    list_display = ('name', 'hp', wepinfo, arminfo, 'gold')
+
 
 class ShopInline(admin.StackedInline):
-    model = models.Shop
+    model = Shop
     fields = ('name',)
     readonly_fields = ('name',)
     has_add_permission = lambda s,r,o=None: False
@@ -69,8 +69,9 @@ class ShopInline(admin.StackedInline):
     has_delete_permission = lambda s,r,o=None: False
     extra = 0
 
+
 class CharInline(admin.StackedInline):
-    model = models.Character
+    model = Character
     fields = (('user_account', 'inventory_mode'),)
     readonly_fields = ('user_account', 'inventory_mode')
     has_add_permission = lambda s,r,o=None: False
@@ -78,23 +79,36 @@ class CharInline(admin.StackedInline):
     has_delete_permission = lambda s,r,o=None: False
     extra = 0
 
+
 def inventory_owners(inventory):
     owners = []
-    owners.extend(models.Character.objects.filter(inventory__pk=inventory.pk))
-    owners.extend(models.Shop.objects.filter(inventory__pk=inventory.pk))
+    owners.extend(Character.objects.filter(inventory__pk=inventory.pk))
+    owners.extend(Shop.objects.filter(inventory__pk=inventory.pk))
     return ''.join(['['+str(owner)+'] ' for owner in owners])
 
+class InventoryItemInfoInline(admin.TabularInline):
+    model = InventoryItemInfo
+    extra = 0
+
+@admin.register(Inventory)
 class InventoryAdmin(admin.ModelAdmin):
     list_display = ('__str__', inventory_owners)
     inlines = [ShopInline, CharInline, InventoryItemInfoInline]
 
 
+def inventory_link(shop):
+    link = reverse('admin:game_inventory_change', args=(shop.inventory.id,))
+    return '<a href="{}">{}</a>'.format(link, shop.inventory)
+inventory_link.allow_tags = True
+inventory_link.short_description = 'Inventory'
+
+@admin.register(Shop)
 class ShopAdmin(admin.ModelAdmin):
-    #list_display = ('name', 'inventory')
-    #list_editable = ('inventory',)
-    search_fields = ('name', )
+    search_fields = ('name',)
+    list_display = ('name', 'item_table', inventory_link)    
 
 
+@admin.register(GameViewProperty)
 class GameViewPropertyAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'char', 'name', 'value')
 
@@ -108,13 +122,14 @@ def get_recipe_products(recipe):
 get_recipe_products.short_description = 'Products'
 
 class RecipeIngredientInfoInline(admin.TabularInline):
-    model = models.RecipeIngredientInfo
+    model = RecipeIngredientInfo
     extra = 0
 
 class RecipeProductInfoInline(admin.TabularInline):
-    model = models.RecipeProductInfo
+    model = RecipeProductInfo
     extra = 0
 
+@admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ('name', get_recipe_products, get_recipe_ingredients)
     inlines = (RecipeIngredientInfoInline, RecipeProductInfoInline)
@@ -124,23 +139,17 @@ def get_goto_locations(location):
     return ', '.join(location.can_goto_views.values_list('name', flat=True))
 get_goto_locations.short_description = 'Allowed locations to move to'
 
+@admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
     list_display = ('name', get_goto_locations)
     
 
 
 
-admin.site.register(models.Character)
-admin.site.register(models.Monster, MonsterAdmin)
-admin.site.register(models.ActiveMonster)
-admin.site.register(models.Item, ItemAdmin)
-admin.site.register(models.ItemProperty)#, ItemPropertyAdmin)
-admin.site.register(models.ItemAction)
-admin.site.register(models.ItemTable)
-admin.site.register(models.ItemTableItemInfo)
-admin.site.register(models.Shop, ShopAdmin)
-admin.site.register(models.Inventory, InventoryAdmin)
-admin.site.register(models.InventoryItemInfo)
-admin.site.register(models.GameViewProperty, GameViewPropertyAdmin)
-admin.site.register(models.Recipe, RecipeAdmin)
-admin.site.register(models.Location, LocationAdmin)
+admin.site.register(Character)
+admin.site.register(ActiveMonster)
+#admin.site.register(ItemProperty)
+admin.site.register(ItemAction)
+admin.site.register(ItemTable)
+admin.site.register(ItemTableItemInfo)
+#admin.site.register(InventoryItemInfo)
