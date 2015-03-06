be416c006db820198dd56d04d4b86ea90b58dfa6
QasimK <QasimK@users.noreply.github.com>
2015-03-06 10:52:33 +0000

    Cleanup admin code

--- wotw_project/game/admin.py
+++ wotw_project/game/admin.py
@@ -4,32 +4,19 @@ from django.core.urlresolvers import reverse
 from game.models import *
 
 
-class ItemPropertyInfoInline(admin.TabularInline):
-    model = ItemProperty
-    extra = 1
-    
-class ItemItemActionInfoInline(admin.TabularInline):
-    model = ItemItemActionInfo
-    extra = 1
-
-def item_properties(item):
-    props = item.itemproperty_set.all()
-    s = ""
-    for prop in props:
-        s += "[{}: {}] ".format(prop.get_name_display(), str(prop.value))
-    return s
-
-def item_actions(item):
-    iiais = item.itemitemactioninfo_set.all()
-    s = ""
-    for iiai in iiais:
-        s += "[{}] ".format(iiai.display_text)
-    return s
-
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
+    class ItemPropertyInfoInline(admin.TabularInline):
+        model = ItemProperty
+        extra = 1
+    
+    class ItemItemActionInfoInline(admin.TabularInline):
+        model = ItemItemActionInfo
+        extra = 1
+    
     list_display = ('name', 'is_unlimited_stack', 'max_stack_size',
-                   'is_soulbound', item_properties, item_actions)
+                   'is_soulbound', 'get_item_properties', 'get_item_actions')
+    
     search_fields = ('name',)
     list_filter = ('itemproperty__name', 'is_unlimited_stack')
     fields = (
@@ -38,26 +25,37 @@ class ItemAdmin(admin.ModelAdmin):
         ('is_soulbound',)
     )
     inlines = [ItemPropertyInfoInline, ItemItemActionInfoInline]
+    
+    def get_item_properties(self, item):
+        props = item.itemproperty_set.all()
+        return ' '.join(['[{}: {}]'.format(prop.get_name_display(), prop.value)
+                         for prop in props])
+    get_item_properties.short_description = 'Item properties'
+    
+    def get_item_actions(self, item):
+        iiais = item.itemitemactioninfo_set.all()
+        return ' '.join(['[{}]'.format(iiai.display_text) for iiai in iiais])
+    get_item_actions.short_description = 'Item actions'
 
-
-def wepinfo(obj):
-    s = "%s (%s)" % (str(obj.weapon), str(obj.weapon.prop_damage))
-    if obj.weapon.is_soulbound:
-        s += "*"
-    return s
-wepinfo.short_description = "Weapon (damage)"
-
-def arminfo(obj):
-    s = "%s (%s)" % (str(obj.armour), str(obj.armour.prop_damage_absorbed))
-    if obj.armour.is_soulbound:
-        s += "*"
-    return s
-arminfo.short_description = "Armour (damage absorbed)"
 
 @admin.register(Monster)
 class MonsterAdmin(admin.ModelAdmin):
     search_fields = ('name',)
-    list_display = ('name', 'hp', wepinfo, arminfo, 'gold')
+    list_display = ('name', 'hp', 'get_wepinfo', 'get_arminfo', 'gold')
+
+    def get_wepinfo(self, obj):
+        s = "%s (%s)" % (str(obj.weapon), str(obj.weapon.prop_damage))
+        if obj.weapon.is_soulbound:
+            s += "*"
+        return s
+    get_wepinfo.short_description = "Weapon (damage)"
+    
+    def get_arminfo(self, obj):
+        s = "%s (%s)" % (str(obj.armour), str(obj.armour.prop_damage_absorbed))
+        if obj.armour.is_soulbound:
+            s += "*"
+        return s
+    get_wepinfo.short_description = "Armour (damage absorbed)"
 
 
 @admin.register(Shop)
@@ -83,47 +81,6 @@ class ShopAdmin(admin.ModelAdmin):
     inventory_link.allow_tags = True
 
 
-class ShopInline(admin.StackedInline):
-    model = Shop
-    fields = ('name',)
-    readonly_fields = ('name',)
-    has_add_permission = lambda s,r,o=None: False
-    #has_change_permission = lambda s,r,o=None: False
-    has_delete_permission = lambda s,r,o=None: False
-    extra = 0
-
-
-class CharInline(admin.StackedInline):
-    model = Character
-    fields = (('user_account', 'inventory_mode'),)
-    readonly_fields = ('user_account', 'inventory_mode')
-    has_add_permission = lambda s,r,o=None: False
-    #has_change_permission = lambda s,r,o=None: False
-    has_delete_permission = lambda s,r,o=None: False
-    extra = 0
-
-
-def inventory_owners(inventory):
-    owners = []
-    owners.extend(Character.objects.filter(inventory__pk=inventory.pk))
-    owners.extend(Shop.objects.filter(inventory__pk=inventory.pk))
-    return ''.join(['['+str(owner)+'] ' for owner in owners])
-
-class InventoryItemInfoInline(admin.TabularInline):
-    model = InventoryItemInfo
-    extra = 0
-
-@admin.register(Inventory)
-class InventoryAdmin(admin.ModelAdmin):
-    list_display = ('__str__', inventory_owners)
-    inlines = [ShopInline, CharInline, InventoryItemInfoInline]
-
-
-@admin.register(GameViewProperty)
-class GameViewPropertyAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'char', 'name', 'value')
-
-
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     class RecipeIngredientInfoInline(admin.TabularInline):
@@ -146,14 +103,54 @@ class RecipeAdmin(admin.ModelAdmin):
     get_recipe_products.short_description = 'Products'
 
 
-def get_goto_locations(location):
-    return ', '.join(location.can_goto_views.values_list('name', flat=True))
-get_goto_locations.short_description = 'Allowed locations to move to'
-
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
-    list_display = ('name', get_goto_locations)
+    list_display = ('name', 'get_goto_locations')
+
+    def get_goto_locations(self, location):
+        return ', '.join(location.can_goto_views.values_list('name', flat=True))
+    get_goto_locations.short_description = 'Allowed locations to move to'
+
+
+@admin.register(Inventory)
+class InventoryAdmin(admin.ModelAdmin):
+    class ShopInline(admin.StackedInline):
+        model = Shop
+        fields = ('name',)
+        readonly_fields = ('name',)
+        has_add_permission = lambda s,r,o=None: False
+        #has_change_permission = lambda s,r,o=None: False
+        has_delete_permission = lambda s,r,o=None: False
+        extra = 0
     
+    
+    class CharInline(admin.StackedInline):
+        model = Character
+        fields = (('user_account', 'inventory_mode'),)
+        readonly_fields = ('user_account', 'inventory_mode')
+        has_add_permission = lambda s,r,o=None: False
+        #has_change_permission = lambda s,r,o=None: False
+        has_delete_permission = lambda s,r,o=None: False
+        extra = 0
+    
+    class InventoryItemInfoInline(admin.TabularInline):
+        model = InventoryItemInfo
+        extra = 0
+    
+    list_display = ('__str__', 'get_inventory_owners')
+    
+    inlines = [ShopInline, CharInline, InventoryItemInfoInline]
+    
+    def get_inventory_owners(self, inventory):
+        owners = []
+        owners.extend(Character.objects.filter(inventory__pk=inventory.pk))
+        owners.extend(Shop.objects.filter(inventory__pk=inventory.pk))
+        return ''.join(['['+str(owner)+'] ' for owner in owners])
+
+
+@admin.register(GameViewProperty)
+class GameViewPropertyAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'char', 'name', 'value')
 
 
 
